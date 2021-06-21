ca13a51ab9104a3bc43636d80714d9af423023a8
lowking <lowking@sina.cn>
2021-06-21 14:18:41 +0800

    add opencard
    (cherry picked from commit 08a4432142355c23aaf1168743e723bc610db794)

--- commands/compareFile.sh
+++ commands/compareFile.sh
@@ -3,13 +3,14 @@ scriptHomePath="$HOME/git-webhook"
 f1=${scriptHomePath}/task/shopid.txt
 f2=${scriptHomePath}/task/shopid.pre.txt
 wget -O ${f1} https://gitee.com/curtinlv/Public/raw/master/OpenCrad/shopid.txt
-file1=`md5sum ${f1}`
-file2=`md5sum ${f2}`
-if [[ "$file1" == "$file2" ]]
+file1=`md5sum ${f1}|awk '{$1}'`
+echo ${file1}
+file2=`md5sum ${f2}|awk '{$1}'`
+echo ${file2}
+if [[ ${file1} = ${file2} ]]
 then
     echo "Files have the same content"
 else
     # 文件内容更新，执行入会
     cp -f ${f1} ${f2}
-    bash ${scriptHomePath}/task/openCard.sh
 fi
