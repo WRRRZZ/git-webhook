a55f3a8cd44a4006a673974850cbf800fc72b32a
lowking <lowking@sina.cn>
2021-06-21 14:32:34 +0800

    add opencard

--- commands/compareFile.sh
+++ commands/compareFile.sh
@@ -3,9 +3,9 @@ scriptHomePath="$HOME/git-webhook"
 f1=${scriptHomePath}/task/shopid.txt
 f2=${scriptHomePath}/task/shopid.pre.txt
 wget -O ${f1} https://gitee.com/curtinlv/Public/raw/master/OpenCrad/shopid.txt
-file1=`md5sum ${f1}|awk '{$1}'`
+file1=`md5sum ${f1}|awk '{print $1}'`
 echo ${file1}
-file2=`md5sum ${f2}|awk '{$1}'`
+file2=`md5sum ${f2}|awk '{print $1}'`
 echo ${file2}
 if [[ ${file1} = ${file2} ]]
 then
@@ -13,4 +13,5 @@ then
 else
     # 文件内容更新，执行入会
     cp -f ${f1} ${f2}
+    bash ${scriptHomePath}/task/openCard.sh
 fi
