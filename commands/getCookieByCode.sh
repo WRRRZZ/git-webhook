f81fa767f4e2ef910fcdfa533cad3f0dd42ae3e1
lowking <lowking@sina.cn>
2021-06-26 02:48:41 +0800

    support update cookie by sms
    (cherry picked from commit b4ca12e96f6be9d1234f96a1e5c8746bab48bbaa)

--- commands/getCookieByCode.sh
+++ commands/getCookieByCode.sh
@@ -3,5 +3,6 @@ scriptHomePath="$HOME/git-webhook"
 telephone=$1
 echo "██开始发送验证码获取ck【${telephone}】"
 (
+    cd ${scriptHomePath}/utils
     export TELEPHONE=${telephone} && python3 ${scriptHomePath}/utils/getJdCookie.py
 )&
