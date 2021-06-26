4b0c300da15058f90196c48ddff89d7323e50b71
lowking <lowking@sina.cn>
2021-06-26 17:36:14 +0800

    optimize
    (cherry picked from commit 0f1a2419bb53e89423ee28385e7a28cfc2b1772b)

--- commands/getCookieByCode.sh
+++ commands/getCookieByCode.sh
@@ -5,5 +5,5 @@ echo "██开始发送验证码获取ck【${telephone}】"
 (
     cd ${scriptHomePath}/utils
     touch ${scriptHomePath}/tmp/${telephone}.code
-    export TELEPHONE=${telephone} && nohup python3 -u ${scriptHomePath}/utils/getJdCookie.py |ts > ${scriptHomePath}/logs/getCookieByCode.log 2>&1&
+    export TELEPHONE=${telephone} && nohup python3 -u ${scriptHomePath}/utils/getJdCookie.py |ts >> ${scriptHomePath}/logs/getCookieByCode.log 2>&1&
 )&
