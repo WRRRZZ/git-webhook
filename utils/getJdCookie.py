db126f9a8d64cedbf8211ee9d8618469dd4ff99a
lowking <lowking@sina.cn>
2021-06-26 11:10:47 +0800

    support update cookie by sms
    (cherry picked from commit 3dbd2ac6c6dfa3da4523b2a4ca300480ebb4d933)

--- utils/getJdCookie.py
+++ utils/getJdCookie.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import subprocess
 import json, os, sys
 
 # git-webhook 主目录
