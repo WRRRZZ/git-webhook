bb62bc7de3b6b6e205f0d9d26969e723d4441393
lowking <lowking@sina.cn>
2021-07-01 21:53:07 +0800

    optimize notify
    (cherry picked from commit c22c1fd7e83861563dd269c72841d88b254a84db)

--- commands/notify.sh
+++ commands/notify.sh
@@ -6,10 +6,16 @@ content=${3}
 if [[ -n "$targetDk" ]]
 then
     qywxKey=`docker exec ${targetDk} /bin/sh -c 'echo $QYWX_KEY'`
-    export QYWX_KEY=${qywxKey} && node ${scriptHomePath}/commands/doSendNotify.js "${title}" "${content}"
+    if [[ -n ${qywxKey} ]]
+    then
+        export QYWX_KEY=${qywxKey} && node ${scriptHomePath}/commands/doSendNotify.js "${title}" "${content}"
+    fi
 
     qywxAm=`docker exec ${targetDk} /bin/sh -c 'echo $QYWX_AM'`
-    export QYWX_AM=${qywxAm} && node ${scriptHomePath}/commands/doSendNotify.js "${title}" "${content}"
+    if [[ -n ${qywxAm} ]]
+    then
+        export QYWX_AM=${qywxAm} && node ${scriptHomePath}/commands/doSendNotify.js "${title}" "${content}"
+    fi
 else
     echo "参数错误！"
 fi
