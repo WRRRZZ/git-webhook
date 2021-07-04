9fdadee1b67597c7cdf1aefbde7b8469a3c1cf94
lowking <lowking@sina.cn>
2021-07-04 13:55:32 +0800

    add cashHelp.sh
    (cherry picked from commit 6a25e80cbb0a4ca6512671211b0d03eaf8a9a54f)

---
+++ task/cashHelp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 初始化配置
+scriptHomePath="$HOME/git-webhook"
+zjdHomePath="/home/lowking/JDOpenCard"
+declare -A dockers
+while read line;
+do
+    dockers[${#dockers[*]}]=${line}
+    echo ${line}
+done<${scriptHomePath}/task/zjddockers.list
+# 获取需要助力的pin
+pins=""
+while read pin;
+do
+    if [[ -z ${pins} ]]
+    then
+        pins=${pin}
+    else
+        pins="${pins},${pin}"
+    fi
+done<${scriptHomePath}/task/zjdpins.list
+# 获取贡献助力的ck
+cks=""
+for dk in ${dockers[@]};
+do
+    while read ck;
+    do
+        if [[ -z ${cks} ]]
+        then
+            cks=${ck}
+        else
+            cks="${cks}&${ck}"
+        fi
+    done<${scriptHomePath}/cookies.list.${dk}
+done
+# 调用脚本执行
+echo -e "\n██获取到的ck：${cks}\n██助力给：${pins}" |ts >> ${scriptHomePath}/logs/cashHelp.log 2>&1&
+export JD_COOKIE=${cks} && export cash_zlzh=${pins} && nohup python3 -u ${zjdHomePath}/jd_cashHelp.py |ts >> ${scriptHomePath}/logs/cashHelp.log 2>&1&
