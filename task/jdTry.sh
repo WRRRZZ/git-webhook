cc06b7d33e335a50b96125880c2eb51c63a145dc
lowking <lowking@sina.cn>
2021-07-06 21:57:42 +0800

    fixed jdTry.sh
    (cherry picked from commit 19810e25b20d64d6ece44710c3e8bf56fcdbac1d)

--- task/jdTry.sh
+++ task/jdTry.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 dk=$1
 index=$2
+minPrice=$3
 scriptHomePath="$HOME/git-webhook"
 # 初始化配置
-ck=$(sed -n "'"${index}","${index}"p'" ${scriptHomePath}/cookies.list.${dk})
+ck=$(sed -n ${index}","${index}"p" ${scriptHomePath}/cookies.list.${dk})
+qywxAm=`docker exec ${dk} /bin/sh -c 'echo $QYWX_AM'`
 echo ${ck}
 # 调用jd_try
-#export QYWX_AM=${qywxAm} && export JD_COOKIE=${JD_COOKIE} &&
+export JD_TRY_MIN_PRICE = ${minPrice} && export QYWX_AM=${qywxAm} && export JD_COOKIE=${ck} && node ${scriptHomePath}/task/jd_try.js |ts >> ${scriptHomePath}/logs/jd_try.log
