4af66231aecce0dbd7afa0d8725172b249ef98c0
lowking <lowking@sina.cn>
2021-07-06 21:58:49 +0800

    fixed jdTry.sh
    (cherry picked from commit 969f3d6e82d3ffd30ae7420d8b5b8319a683f40d)

--- task/jdTry.sh
+++ task/jdTry.sh
@@ -8,4 +8,4 @@ ck=$(sed -n ${index}","${index}"p" ${scriptHomePath}/cookies.list.${dk})
 qywxAm=`docker exec ${dk} /bin/sh -c 'echo $QYWX_AM'`
 echo ${ck}
 # 调用jd_try
-export JD_TRY_MIN_PRICE = ${minPrice} && export QYWX_AM=${qywxAm} && export JD_COOKIE=${ck} && node ${scriptHomePath}/task/jd_try.js |ts >> ${scriptHomePath}/logs/jd_try.log
+export JD_TRY_MIN_PRICE=${minPrice} && export QYWX_AM=${qywxAm} && export JD_COOKIE=${ck} && node ${scriptHomePath}/task/jd_try.js |ts >> ${scriptHomePath}/logs/jd_try.log
